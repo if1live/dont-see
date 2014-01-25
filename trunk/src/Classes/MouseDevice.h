cb6d5ab13b60d3dd04f01d78941eb0b198e8c966
mang9th <mang9th@9717bcc1-1978-72e4-6d8e-9e18e0574ad3>
2014-01-25 12:50:01 +0000

    * MouseDevice 추가(누락 파일 추가)

---
+++ trunk/src/Classes/MouseDevice.h
@@ -0,0 +1,37 @@
+#pragma once
+
+class MouseDevice
+{
+public:
+	static MouseDevice* sharedDevice() {
+		static MouseDevice device;
+		return &device;
+	}
+
+	MouseDevice() {
+		m_hwnd = FindWindow(0, L"HelloCpp - 1024x768 - 1.00");
+	}
+
+	void Update();
+
+	inline POINT GetMousePos() const
+	{
+		return m_point;
+	}
+
+private:
+	HWND m_hwnd;
+	POINT m_point;
+};
+
+inline void MouseDevice::Update() {
+	GetCursorPos(&m_point);
+	//CCLOG("CursorPos:%d, %d", m_point.x, m_point.y);
+	ScreenToClient(m_hwnd, &m_point);
+	//CCLOG("RelativePos:%d, %d", m_point.x, abs(768 - m_point.y));
+
+	m_point.x = m_point.x - (SCREEN_WIDTH / 2);
+	m_point.y = abs(768 - m_point.y) - (SCREEN_HEIGHT / 2);
+
+	//CCLOG("CalcPos:%d, %d", m_point.x, m_point.y);
+}
