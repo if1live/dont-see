ca72b987244e01db8a3ad870805d2a37d63620a7
libsora25@gmail.com <libsora25@gmail.com@9717bcc1-1978-72e4-6d8e-9e18e0574ad3>
2014-01-25 03:00:45 +0000

    해상도 변경. 그러면 원점이 대충 해결된다

--- trunk/src/proj.win32/main.cpp
+++ trunk/src/proj.win32/main.cpp
@@ -18,7 +18,7 @@ int main()
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
     eglView->setViewName("HelloCpp");
     //eglView->setFrameSize(2048, 1536);
-	eglView->setFrameSize(1024, 768);
+	eglView->setFrameSize(960, 640);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
     eglView->setFrameZoomFactor(1.0f);
