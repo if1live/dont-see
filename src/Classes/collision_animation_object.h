66ba849da720afcabf871ff8c4f9be1b9ba55953
libsora25 <libsora25@gmail.com>
2014-01-25 16:20:39 +0000

    충돌시 이미지 바뀌는거(소화전, 쓰레기통) 클래스 하나 추가

---
+++ src/Classes/collision_animation_object.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "TmxObject.h"
+
+class CollisionAnimationObject : public TmxObject {
+public:
+	static CollisionAnimationObject* create(cocos2d::CCDictionary* dict);
+
+	CollisionAnimationObject(cocos2d::CCDictionary *dict);
+
+	virtual b2Body *createBody();
+	virtual void update(float dt);
+
+private:
+};
