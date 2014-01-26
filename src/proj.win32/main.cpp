ad09ef2b60e49df9e559495d883671a58943a211
libsora25 <libsora25@gmail.com>
2014-01-26 05:11:55 +0000

    콘솔제거. 게임 이름 변경

--- src/proj.win32/main.cpp
+++ src/proj.win32/main.cpp
@@ -5,20 +5,19 @@
 
 USING_NS_CC;
 
-/*
+
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                        HINSTANCE hPrevInstance,
                        LPTSTR    lpCmdLine,
                        int       nCmdShow)
-					   */
-int main()
+//int main()
 {
 	srand(time(NULL));
 
     // create the application instance
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
-    eglView->setViewName("HelloCpp");
+    eglView->setViewName("Far Sight");
     //eglView->setFrameSize(2048, 1536);
 	eglView->setFrameSize(SCREEN_WIDTH, SCREEN_HEIGHT);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
